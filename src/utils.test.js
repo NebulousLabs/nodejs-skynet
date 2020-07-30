5b5572548ce39c8bfdbcc533a9eed7ee141b8e2d
Marcin S <scatman@bu.edu>
2020-07-30 19:52:39 +0200

    Fix bug in URL creation and add test

---
+++ src/utils.test.js
@@ -0,0 +1,23 @@
+const { defaultPortalUrl, makeUrl, trimSiaPrefix, uriSkynetPrefix } = require("./utils.js");
+
+const portalUrl = defaultPortalUrl;
+const skylink = "XABvi7JtJbQSMAcDwnUnmp2FKDPjg8_tTTFP4BwMSxVdEg";
+
+describe("makeUrl", () => {
+  it("should return correctly formed URLs", () => {
+    expect(makeUrl(portalUrl, "/")).toEqual(`${portalUrl}/`);
+    expect(makeUrl(portalUrl, "/skynet")).toEqual(`${portalUrl}/skynet`);
+    expect(makeUrl(portalUrl, "/skynet/")).toEqual(`${portalUrl}/skynet/`);
+
+    expect(makeUrl(portalUrl, "/", skylink)).toEqual(`${portalUrl}/${skylink}`);
+    expect(makeUrl(portalUrl, "/skynet", skylink)).toEqual(`${portalUrl}/skynet/${skylink}`);
+    expect(makeUrl(portalUrl, "//skynet/", skylink)).toEqual(`${portalUrl}/skynet/${skylink}`);
+  });
+});
+
+describe("trimSiaPrefix", () => {
+  it("should correctly trim the sia prefix", () => {
+    expect(trimSiaPrefix(skylink)).toEqual(skylink);
+    expect(trimSiaPrefix(`${uriSkynetPrefix}${skylink}`)).toEqual(skylink);
+  });
+});
