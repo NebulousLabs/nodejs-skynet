07d186b803ceafecdf312407fabd03676b7972c9
Marcin S <scatman@bu.edu>
2020-08-28 16:59:06 +0200

    Remove administrative API

--- src/client.js
+++ src/client.js
@@ -51,12 +51,6 @@ class SkynetClient {
 module.exports = { SkynetClient };
 
 // Get the following files to run or the client's methods won't be defined.
-require("./blocklist.js");
-require("./convert.js");
 require("./download.js");
 require("./encryption.js");
-require("./list.js");
-require("./pin.js");
-require("./portals.js");
-require("./stats.js");
 require("./upload.js");
